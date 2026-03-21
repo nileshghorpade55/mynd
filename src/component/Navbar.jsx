428bd8e3ae5d9b798774c0577a57f726e7615dd7
nileshghorpade55 <87520014+nileshghorpade55@users.noreply.github.com>
2026-03-21 10:40:03 +0530

    changes done for Routing

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,66 +1,69 @@
+import { useState } from "react";
 import logo from "../assets/image/logoim.jpg";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
 
-      <Link className="navbar-brand d-flex align-items-center" to="/">
-  <img 
-    src={logo} 
-    alt="Logo" 
-    style={{ 
-      height: "90px",        // Make logo even bigger
-      width: "auto", 
-      marginRight: "20px",  // Negative margin to pull text closer to the icon
-      objectFit: "contain"
-    }} 
-  />
-  <div className="d-flex flex-column" style={{ marginLeft: "0px" }}>
-    <span style={{ 
-      fontSize: "1.4rem",    
-      fontWeight: "800", 
-      lineHeight: "0.9",
-      color: "#00B4D8",
-      letterSpacing: "0.5px"
-    }}>
-      DRYCLEANER
-    </span>
-    <span style={{ 
-      fontSize: "0.7rem", 
-      fontWeight: "600",
-      color: "#555",
-      letterSpacing: "1px",
-      marginTop: "2px"
-    }}>
-      LAUNDRY SERVICE
-    </span>
-  </div>
-</Link>
+        {/* Logo Section */}
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img 
+            src={logo} 
+            alt="Logo" 
+            style={{ 
+              height: "90px",
+              width: "auto",
+              marginRight: "20px",
+              objectFit: "contain"
+            }} 
+          />
+          <div className="d-flex flex-column">
+            <span style={{ 
+              fontSize: "1.4rem",    
+              fontWeight: "800", 
+              lineHeight: "0.9",
+              color: "#00B4D8",
+              letterSpacing: "0.5px"
+            }}>
+              DRYCLEANER
+            </span>
+            <span style={{ 
+              fontSize: "0.7rem", 
+              fontWeight: "600",
+              color: "#555",
+              letterSpacing: "1px",
+              marginTop: "2px"
+            }}>
+              LAUNDRY SERVICE
+            </span>
+          </div>
+        </Link>
+
+        {/* Hamburger Button */}
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={() => setIsOpen(!isOpen)}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
+        {/* Menu */}
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/" onClick={() => setIsOpen(false)}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/pricing">Pricing</Link>
+              <Link className="nav-link" to="/pricing" onClick={() => setIsOpen(false)}>Pricing</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#footer">Contact</a>
+              <a className="nav-link" href="#footer" onClick={() => setIsOpen(false)}>Contact</a>
             </li>
           </ul>
         </div>
